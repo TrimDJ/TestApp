709e48f41dc097595b9022c3371753c4e7462b3b
DjambulatStambulov <Trim_D_J@mail.rru>
2022-01-27 16:19:44 +0300

    Custom Table View Cell

---
+++ TestApp/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  TestApp
+//
+//  Created by  Джамбулат Стамбулов on 27.01.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imagePerson: UIImageView!
+    @IBOutlet weak var firstNamePerson: UILabel!
+    @IBOutlet weak var secondNamePerson: UILabel!
+    @IBOutlet weak var companyPerson: UILabel!
+    
+
+}
--- TestApp/FirstViewController.swift
+++ TestApp/FirstViewController.swift
@@ -21,10 +21,16 @@ class FirstViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = person[indexPath.row]
-        cell.imageView?.image = UIImage(named: person[indexPath.row])
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+        cell.firstNamePerson.text = person[indexPath.row]
+        cell.imagePerson.image = UIImage(named: person[indexPath.row])
+        cell.imagePerson.layer.cornerRadius = cell.imagePerson.frame.size.height / 2
+        cell.imagePerson.clipsToBounds = true
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 65
     }
 }
